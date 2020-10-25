dbee8b9ac671a9cf6e51cfbfba581da747550e59
johngodlee <johngodlee@gmail.com>
2020-10-25 22:38:23 +0000

    Prettified tables

---
+++ indval_fmt.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# Multirow indicator species table clusters
+sed '9s/^1\s\&/\\multirow{3}{*}{1} \&/g' out/indval.tex |\
+	sed '10s/^\s\+1\s\&/\&/g' |\
+	sed '11s/^\s\+1\s\&/\&/g' |\
+	sed '13s/^2\s\&/\\multirow{3}{*}{2} \&/g' |\
+	sed '14s/^\s\+2\s\&/\&/g' |\
+	sed '15s/^\s\+2\s\&/\&/g' |\
+	sed '17s/^3\s\&/\\multirow{3}{*}{3} \&/g' |\
+	sed '18s/^\s\+3\s\&/\&/g' |\
+	sed '19s/^\s\+3\s\&/\&/g' |\
+	sed '21s/^4\s\&/\\multirow{3}{*}{4} \&/g' |\
+	sed '22s/^\s\+4\s\&/\&/g' |\
+	sed '23s/^\s\+4\s\&/\&/g' > out/indval_fmt.tex
