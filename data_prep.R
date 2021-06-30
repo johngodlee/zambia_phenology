0185717f946d76c3b8f585fa412736f72831f2a7
John L. Godlee <johngodlee@gmail.com>
2021-06-30 10:49:16 +0100

    Updated data to seosaw v2.12

--- data_prep.R
+++ data_prep.R
@@ -11,25 +11,29 @@ library(tibble)
 source("functions.R")
 
 # Import data
-plots <- read.csv("dat/plots_v2.7.csv")
+plots <- read.csv("dat/plots_v2.12.csv")
+plots_sp <- read.csv("dat/plots_spatial_v2.12.csv")
 
-stems <- read.csv("dat/stems_latest_v2.7.csv")
+stems <- read.csv("dat/stems_iluaii_v2.12.csv")
 
 # Subset plots and data fields - Zambia ILUAii data only
 plots_fil_sf <- plots %>% 
   filter(prinv == "Siampale A.") %>%  # Filter ILUAii plots
+  left_join(., plots_sp, by = "plot_id") %>%
+  mutate(n_stems_ge10_ha = n_stems_ge10 / plot_area) %>% 
   dplyr::select(
     plot_id, plot_cluster, longitude_of_centre, latitude_of_centre, 
-    richness, shannon, simpson, evenness, n_stems_gt10_ha, ba_ha, agb_ha,
+    richness, shannon, simpson, evenness, n_stems_ge10_ha, ba_ha, agb_ha,
     mat = bio1, diurnal_temp_range = bio2, map = bio12,
-    clay = CLYPPT, sand = SNDPPT, cec = CECSOL,
-    last_census_date) %>%  # Select columns
+    clay = soil_clay, sand = soil_sand, cec = soil_cation_ex_cap, 
+    census_date) %>%  # Select columns
   group_by(plot_cluster) %>%  # Group by plot cluster
   summarise(
     plot_id = paste0(plot_id, collapse = ","),
     longitude_of_centre = mean(longitude_of_centre, na.rm = TRUE),
     latitude_of_centre = mean(latitude_of_centre, na.rm = TRUE),
-    n_stems_gt10_ha = mean(n_stems_gt10_ha, na.rm = TRUE),
+    census_date = first(census_date),
+    n_stems_ge10_ha = mean(n_stems_ge10_ha, na.rm = TRUE),
     ba_ha = mean(ba_ha, na.rm = TRUE),
     agb_ha = mean(agb_ha, na.rm = TRUE),
     mat = mean(mat, na.rm = TRUE),
@@ -37,24 +41,20 @@ plots_fil_sf <- plots %>%
     map = mean(map, na.rm = TRUE),
     clay = mean(clay, na.rm = TRUE),
     sand = mean(sand, na.rm = TRUE),
-    cec = mean(cec, na.rm = TRUE),
-    last_census_date = first(last_census_date)) %>%  # Summarise by plot cluster
+    cec = mean(cec, na.rm = TRUE)) %>%  # Summarise by plot cluster
   st_as_sf(., coords = c("longitude_of_centre", "latitude_of_centre")) %>%  # Make spatial
   `st_crs<-`(4326) %>%  # Assign CRS
   mutate(plot_id_vec = strsplit(as.character(plot_id), split = ",")) %>%  # Make column of plot ID vectors 
   mutate(plot_id_length = sapply(.$plot_id_vec, length))  # Get number of plots in cluster
 
 # Get statistics about sites
-census <- unique(plots_fil_sf$last_census_date) 
+census <- unique(plots_fil_sf$census_date) 
 
 n_total_sites <- plots %>%
   filter(prinv == "Siampale A.") %>%
   pull(plot_cluster) %>%
   unique() %>%
   length()
-
-plots_fil_sf <- plots_fil_sf %>% 
-  dplyr::select(-last_census_date)
 
 # Create plot ID / plot Cluster lookup table
 plot_id_lookup <- plots %>% 
@@ -65,11 +65,14 @@ plot_id_lookup <- plots %>%
 stem_size <- 10
 stems_fil <- stems %>%
   inner_join(., plot_id_lookup, by = "plot_id") %>%
-  filter(diam >= stem_size) %>% 
-  filter(plot_cluster %in% plots_fil_sf$plot_cluster)
+  filter(
+    alive == "a",
+    diam >= stem_size,
+    plot_cluster %in% plots_fil_sf$plot_cluster)
 
 # Summarise to only trees
 # Remove duplicated tree measurements
+# Remove unidentified species
 tree_fil <- stems_fil %>%
   filter(!is.na(species_name_clean)) %>%
   group_by(plot_id, tree_id) %>%
@@ -79,6 +82,16 @@ tree_fil <- stems_fil %>%
     diam = sum(diam, na.rm = TRUE),
     ba = sum(ba, na.rm = TRUE),
     agb = sum(agb, na.rm = TRUE))
+
+ntrees <- length(tree_fil$species_name_clean)
+nspindet <- sum(grepl("indet", tree_fil$species_name_clean) & 
+  !grepl("Indet indet", tree_fil$species_name_clean)) 
+perspindet <- nspindet / ntrees * 100
+nuniquespindet <- length(unique(tree_fil$species_name_clean[grepl("indet", tree_fil$species_name_clean)]))
+ngenindet <- sum(grepl("Indet indet", tree_fil$species_name_clean))
+pergenindet <- ngenindet / ntrees * 100
+
+# How many trees weren't ID'd to species?
 
 # Create tree species abundance matrix by plot cluster
 ba_clust_mat <- abMat(tree_fil, site_id = "plot_cluster", 
@@ -104,6 +117,7 @@ ba_clust_mat <- ba_clust_mat[row.names(ba_clust_mat) %in% ba_ha_cluster,]
 sp_ab <- unname(apply(occ_clust_mat, 1, function(x) { 
     sum(x > 1, na.rm = TRUE) 
   }))
+
 sp_ab_cluster <- row.names(occ_clust_mat[sp_ab >= 5,])
 ba_clust_mat <- ba_clust_mat[row.names(ba_clust_mat) %in% sp_ab_cluster,] 
 
@@ -134,5 +148,9 @@ write(
     commandOutput(ba_ha_lim, "baLim"),
     commandOutput(stem_size, "stemSize"),
     commandOutput(census, "censusDate"),
-    commandOutput(n_total_sites, "nTotalSites")),
+    commandOutput(n_total_sites, "nTotalSites"),
+    commandOutput(ntrees, "nTrees"),
+    commandOutput(perspindet, "perSpIndet"),
+    commandOutput(pergenindet, "perGenIndet")
+    ),
   file = "out/data_prep_vars.tex")
